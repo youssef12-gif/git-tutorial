a2b771a00f6ba295117301f0c0c644196c611205
Youssef Emad <ymad88679@gmail.com>
2026-01-29 07:13:50 +0200

    version 2 update

--- plapla.js
+++ plapla.js
@@ -1,2 +1,3 @@
 console.log('I di not care');
 console.log('plaplapal');
+console.log('version 2 update');
--- welcome.js
+++ welcome.js
@@ -1,2 +1,3 @@
 console.log('Hello');
 console.log('strangeeeeeeeee');
+console.log('version 2 update');
